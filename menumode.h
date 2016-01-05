5a0b71fabac79bbf295c27564242e7afe4e24611
CST-hayata <pe3398530@gmail.com>
2016-01-05 09:46:01 +0800

    Basic Menu selection.

--- menumode.h
+++ menumode.h
@@ -2,23 +2,16 @@
 struct{
 	VIEWPORTid vID;
 	SCENEid sID, sID2;
-	OBJECTid cID, tID, lID, spID[3];
+	OBJECTid cID, tID, lID, spID;
 	TEXTid textID;
+	int mode_select;
 	void load(){
 		textID = FAILED_ID;
 		vID = FyCreateViewport(0, 0, 800, 600);
 		FnViewport vp;
 		vp.ID(vID);
 		textID = FyCreateText("Trebuchet MS2", 18, FALSE, FALSE);
-	}
-	void GameAI(int skip){
-
-	}
-	int Movement(BYTE code, BOOL4 value){
-		if (code == FY_Z)return 2;
-		return 1;
-	}
-	void RenderIt(int skip){
+		mode_select = 0;
 		
 		FnScene scene;
 		scene.ID(sID);
@@ -39,13 +32,51 @@ struct{
 		scene.ID(sID2);
 		scene.SetSpriteWorldSize(800, 600);
 		
-		FnSprite sp[3];
-	float sp_color[3][3] = {{0.8, 0.0, 0.4},{0.8, 0.0, 0.4},{0.8, 0.0, 0.4}};
-		spID[0] = scene.CreateObject(SPRITE);
-		sp[0].ID(spID[0]);
-		sp[0].SetSize(200, 40);
-		sp[0].SetColor(sp_color[0]);
-		sp[0].SetPosition(300, 200, 0);
+		FnSprite sp;
+		// START Button
+		spID = scene.CreateObject(SPRITE);
+		sp.ID(spID);
+		sp.SetSize(200, 40);
+		sp.SetImage("Start", 0, NULL, 0, NULL, NULL, MANAGED_MEMORY, false, false);
+		sp.SetPosition(300, 250, 0);
+		// OPTION Button
+		spID = scene.CreateObject(SPRITE);
+		sp.ID(spID);
+		sp.SetSize(200, 40);
+		sp.SetImage("Option", 0, NULL, 0, NULL, NULL, MANAGED_MEMORY, false, false);
+		sp.SetPosition(300, 200, 0);
+		// QUIT Button
+		spID = scene.CreateObject(SPRITE);
+		sp.ID(spID);
+		sp.SetSize(200, 40);
+		sp.SetImage("Quit", 0, NULL, 0, NULL, NULL, MANAGED_MEMORY, false, false);
+		sp.SetPosition(300, 150, 0);
+		// Selection mark
+		spID = scene.CreateObject(SPRITE);
+		sp.ID(spID);
+		sp.SetSize(200, 40);
+		sp.SetImage("Select", 0, NULL, 0, NULL, NULL, MANAGED_MEMORY, false, false);
+		sp.SetPosition(300, 250-50*mode_select, 0);
+	}
+	void GameAI(int skip){
+
+	}
+	int Movement(BYTE code, BOOL4 value){
+		int ret = 1;
+		if (code == FY_DOWN && value) mode_select += 1;
+		else if (code == FY_UP && value) mode_select -= 1;
+		else if (code == FY_Z && mode_select == 0) ret = 2;
+		else if (code == FY_Z && mode_select == 2) FyQuitFlyWin32();
+		
+		if (mode_select < 0) mode_select = 0;
+		if (mode_select > 2) mode_select = 2;
+		
+		return ret;
+	}
+	void RenderIt(int skip){
+		FnSprite sp;
+		sp.ID(spID);
+		sp.SetPosition(300, 250-50*mode_select, 0);
 		
 		FnViewport vp;
 		vp.ID(vID);
@@ -58,8 +89,6 @@ struct{
 		char S[256];
 		sprintf(S, "Lyubu xxxxx Donzo is irreversible!!!\n");
 		text.Write(S, 300, 200, 255, 255, 0);
-		sprintf(S, "press z to start.\n");
-		text.Write(S, 300, 400, 255, 255, 0);
 		text.End();
 	}
 }MM;
