1d71646035e59fbed35b7cf5c6ee44c6fcb7ae69
CST-hayata <pe3398530@gmail.com>
2016-01-10 05:08:16 +0800

    Now can select all 6 characters. Change Select mode character image.

--- selectmode.h
+++ selectmode.h
@@ -3,12 +3,14 @@
 #include <cstdio>
 #include <iostream>
 #include <vector>
-#define TYPESOFC 4
+#define TYPESOFC 6
 string character_name[TYPESOFC] = {
 	"Donzo2",
 	"Lyubu2",
 	"Robber02",
 	"Footman01",
+	"Sorcerer01",
+	"WarLord01"
 };
 
 struct info{
@@ -22,8 +24,9 @@ info INFO[TYPESOFC] = {
 	info({ 200, 12, 20, 20, 140, 25 }),
 	info({ +30, +9, 10, 20, 140, 25 }),
 	info({ +75, +9, 15, 20, 140, 25 }),
+	info({ +50, 12, 10, 20, 140, 25 }),
+	info({ 123, +6, 20, 20, 140, 25 })
 };
-
 struct{
 	VIEWPORTid vID;
 	SCENEid sID;
@@ -35,7 +38,7 @@ struct{
 	int select_character;
 	
 	void load(){
-		character_number = 4;
+		character_number = 6;
 		default_character = 0;
 		select_character = default_character;
 
@@ -76,14 +79,17 @@ struct{
 		int ret = 3;
 		if (value){
 			switch (code){
+			case FY_X:
+				ret = 1;
+				break;
 			case FY_Z:
 				ret = 4;
 				break;
 			case FY_UP:
-				select_character += 1;
+				select_character -= 1;
 				break;
 			case FY_DOWN:
-				select_character -= 1;
+				select_character += 1;
 				break;
 			default:
 				break;
@@ -123,12 +129,11 @@ struct{
 		char S[256], character_info[256];
 		BYTE red = 255, green = 255, blue = 255;
 		int positionX = 300, positionY = 100;
-		sprintf(S, "Choose the character you want\nPress \"Z\" to select character.\n");
+		sprintf(S, "Choose the character you want\nPress \"Z\" to select character.\n Press \"X\" Back to main menu.\n");
 		sprintf(character_info, "Character : %s\nAttack : \t%f\nBlood : \t%d\nSpeed : \t%d\n", 
-															character_name[select_character].c_str(), 
-															INFO[select_character].speed,
-															INFO[select_character].life,
-															INFO[select_character].attnD); // TODO : need characterinformation
+			character_name[select_character].c_str(), INFO[select_character].speed,
+			INFO[select_character].life, INFO[select_character].attnD);
+		// TODO : need characterinformation
 		
 		text.Write(S, positionX, positionY, red, green, blue); // 
 		text.Write(character_info, 300, 350, red, green, blue); // print infomation 
